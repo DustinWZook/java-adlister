570429b70ed59f1e1ffe8feda4dfbf8618636016
Dustin Zook <dustin.w.zook@gmail.com>
2023-01-05 11:35:12 -0600

    working on exercise

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -3,21 +3,34 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.getRequestDispatcher("/login.jsp").forward(request, response);
+        HttpSession session = request.getSession();
+        boolean isAdmin = session.getAttribute("user").equals("admin");
+        if (isAdmin){
+            request.getRequestDispatcher("/profile").forward(request,response);
+//            response.sendRedirect("/WEB-INF/profile");
+        }
     }
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
         boolean validAttempt = username.equals("admin") && password.equals("password");
-
+        HttpSession session = request.getSession();
+        session.setAttribute("user", username);
+//        if (isAdmin){
+//            request.getRequestDispatcher("/profile").forward(request,response);
         if (validAttempt) {
-            response.sendRedirect("/profile");
+//            boolean isAdmin;
+            request.getSession().setAttribute("isAdmin", true);
+            request.getRequestDispatcher("/profile").forward(request,response);
+//            response.sendRedirect("/profile");
         } else {
             response.sendRedirect("/login");
         }
